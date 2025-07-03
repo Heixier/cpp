735d1b80fbc6678828bd93e05d4cb6b24ef880c6
Heixier <rzsiah@gmail.com>
2025-07-03 13:47:55 +0800

    add megaphone; no makefile

---
+++ 42/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+	}
+	std::cout << '\n';
+	return (0);
+}
