36f348b2dd5824a0d841c92bf7df47fb9d6b7b9e
Heixier <rzsiah@gmail.com>
2025-07-21 13:03:05 +0800

    adjust formatting for ex06

--- 42/cpp01/ex06/Harl.cpp
+++ 42/cpp01/ex06/Harl.cpp
@@ -37,10 +37,10 @@ void	Harl::complain(std::string level) const
 	}
 }
 
-void	Harl::debug(void) const { std::cout << "\e[38;5;117;1m[ DEBUG ]\nwah ERP so expensive now lah last time fifty cents only now six dollar how like that\e[0m\n"; }
-void	Harl::info(void) const { std::cout << "\e[38;5;226;1m[ INFO ]\nthis bmw driver no signal just cut in front of me stupid or what make me brake so hard my kopi nearly spill\e[0m\n"; }
-void	Harl::warning(void) const { std::cout << "\e[38;5;208;1m[ WARNING ]\nCOE 100k now just for paper only haven even buy car yet damn stupid sia how ppl afford?\e[0m\n"; }
-void	Harl::error(void) const { std::cout << "\e[38;5;196;1m[ ERROR ]\nwalao this grab driver cut three lanes nearly hit my car want to call TP report him this kind idiot shouldnt have license\e[0m\n"; }
+void	Harl::debug(void) const { std::cout << "\e[38;5;117;1m[ DEBUG ]\nwah ERP so expensive now lah last time fifty cents only now six dollar how like that\e[0m\n\n"; }
+void	Harl::info(void) const { std::cout << "\e[38;5;226;1m[ INFO ]\nthis bmw driver no signal just cut in front of me stupid or what make me brake so hard my kopi nearly spill\e[0m\n\n"; }
+void	Harl::warning(void) const { std::cout << "\e[38;5;208;1m[ WARNING ]\nCOE 100k now just for paper only haven even buy car yet damn stupid sia how ppl afford?\e[0m\n\n"; }
+void	Harl::error(void) const { std::cout << "\e[38;5;196;1m[ ERROR ]\nwalao this grab driver cut three lanes nearly hit my car want to call TP report him this kind idiot shouldnt have license\e[0m\n\n"; }
 
 Harl::Harl(void) { }
 Harl::~Harl(void) { }
