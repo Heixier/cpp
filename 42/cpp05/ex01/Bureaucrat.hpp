756d44c581255c77a86a88b97bae9c41e56c2183
Heixier <rzsiah@gmail.com>
2025-08-16 11:21:39 +0800

    improve testing, start ex01

---
+++ 42/cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+	public:
+		const std::string& getName() const;
+		int getGrade() const;
+
+		void increment();
+		void decrement();
+
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class CannotBeCopiedException: public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		Bureaucrat();
+		Bureaucrat(int grade);
+		Bureaucrat(const std::string& name);
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator= (const Bureaucrat& other);
+		~Bureaucrat();
+
+	private:
+		const std::string m_name;
+		int m_grade;
+};
+
+std::ostream& operator<< (std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
