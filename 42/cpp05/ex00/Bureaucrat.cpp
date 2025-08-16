fb6ff541e9877524f5304a4a91d50d215ab7e5a0
Heixier <rzsiah@gmail.com>
2025-08-16 11:17:24 +0800

    add ex00

--- 42/cpp05/ex00/Bureaucrat.cpp
+++ 42/cpp05/ex00/Bureaucrat.cpp
@@ -1,2 +1,99 @@
 #include "Bureaucrat.hpp"
 
+static void check_grade(int grade)
+{
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+}
+
+Bureaucrat::Bureaucrat(): m_name("Snoopy"), m_grade(150)
+{
+	check_grade(m_grade);
+	std::cout << "Bureaucrat constructed\nName: " << m_name << "\nGrade: " << m_grade << '\n';
+}
+
+Bureaucrat::Bureaucrat(int grade): m_name("Snoopy"), m_grade(grade)
+{
+	check_grade(m_grade);
+	std::cout << "Bureaucrat constructed\nName: " << m_name << "\nGrade: " << m_grade << '\n';
+}
+
+Bureaucrat::Bureaucrat(const std::string& name): m_name(name), m_grade(150)
+{
+	check_grade(m_grade);
+	std::cout << "Bureaucrat constructed\nName: " << m_name << "\nGrade: " << m_grade << '\n';
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade): m_name(name), m_grade(grade)
+{
+	check_grade(m_grade);
+	std::cout << "Bureaucrat constructed\nName: " << m_name << "\nGrade: " << m_grade << '\n';
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other): m_name(other.m_name), m_grade(other.m_grade)
+{
+	std::cout << "Bureaucrat " << m_name << " was copied\n";
+}
+
+Bureaucrat& Bureaucrat::operator= (const Bureaucrat& other)
+{
+	throw CannotBeCopiedException();
+	if (this != &other)
+	{
+		// copying just grade is confusing behaviour because users will righfully assume the whole thing was copied
+		// this can cause weird bugs later on when the name is checked
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat " << m_name << ", grade: " << m_grade << " was fired\n";
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high exception thrown");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low exception thrown");
+}
+
+const char* Bureaucrat::CannotBeCopiedException::what() const throw()
+{
+	return ("This object cannot be copy assigned (usually due to const attributes)!");
+}
+
+void Bureaucrat::increment()
+{
+	if (m_grade == 1)
+		throw Bureaucrat::GradeTooHighException();
+	m_grade--;
+}
+
+void Bureaucrat::decrement()
+{
+	if (m_grade == 150)
+		throw Bureaucrat::GradeTooLowException();
+	m_grade++;
+}
+
+const std::string& Bureaucrat::getName() const
+{
+	return (m_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (m_grade);
+}
+
+std::ostream& operator<< (std::ostream& os, const Bureaucrat& bureaucrat)
+{
+	os << bureaucrat.getName() << ", " << bureaucrat.getGrade() << ".\n";
+	return (os);
+}
---
+++ 42/cpp05/ex00/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <exception>
+#include <unistd.h>
+
+#include "Bureaucrat.hpp"
+#include "colors.hpp"
+
+static void test_create(const std::string& name, int grade)
+{
+	try
+	{
+		std::cout << "\nTrying to create a Bureaucrat named " << name << " with a grade of " << grade << ":\n";
+		Bureaucrat test(name, grade);
+		std::cout << LIGHT_GREEN << "Success!\n" << END;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << RED << e.what() << '\n' << END;
+	}
+}
+
+static void work(Bureaucrat& worker, bool increment)
+{
+	while (true)
+	{
+		std::cout << ICE_BLUE << worker.getName() << "'s grade: " << worker.getGrade() << '\n' << END;
+		try
+		{
+			increment == true ? worker.increment() : worker.decrement();
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << RED << e.what() << '\n' << END;
+			break;
+		}
+		sleep(1);
+	}
+}
+
+int main(void)
+{
+	test_create("John", 2);
+	test_create("Alex", 150);
+	test_create("James", -5);
+	test_create("Jane", 1000000);
+
+	Bureaucrat mary("Mary", 5);
+	work(mary, true);
+
+	Bureaucrat alex("Alex", 145);
+	work(alex, false);
+
+}
