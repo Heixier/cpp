e9ef431449d58c03fe85f727ec658121dd0e21eb
Heixier <rzsiah@gmail.com>
2025-08-19 11:58:37 +0800

    rewrite; add special case handling

--- 42/cpp06/ex00/ScalarConverter.cpp
+++ 42/cpp06/ex00/ScalarConverter.cpp
@@ -3,139 +3,48 @@
 #include <sstream>
 #include <cctype>
 #include <iostream>
+#include <cstdio>
+#include <iomanip>
 
-#define ll long long
-#define ld long double
-
-#define print(str) std::cout << str
-
-#define LIM(THolder, val, op, TTarget) static_cast<THolder>(val) op std::numeric_limits<TTarget>::max()
-
-static bool char_convert(const std::string& input)
+typedef struct s_pseudo
 {
-	std::istringstream iss;
-	char output;
+	const std::string& p_float;
+	const std::string& p_double;
+}	t_pseudo;
 
-	iss.str(input);
-	iss >> output;
-	if (iss.fail() || !iss.eof())
-		return (false);
-	
-	if (std::isprint(output))
-		std::cout << "char: " << output << '\n';
-	else
-		std::cout << "char: Non displayable\n";
-
-	if (LIM(ll, output, >, int) || LIM(ll, output, <, int))
-		std::cout << "int: impossible\n";
-	else
-		std::cout << "int: " << static_cast<int>(output) << '\n';
-
-	if (static_cast<long double>(output) > std::numeric_limits<float>::max() || static_cast<long double>(output) < std::numeric_limits<float>::min())
-		std::cout << "float: nanf\n";
-	else
-		std::cout << "float: " << static_cast<float>(output) << '\n';
-		
-	if (static_cast<long double>(output) > std::numeric_limits<double>::max() || static_cast<long double>(output) < std::numeric_limits<double>::min())
-		std::cout << "double: nan\n";
-	else
-		std::cout << "double: " << static_cast<double>(output) << '\n';
-	return (true);
+static void print_pseudo(const t_pseudo& pseudos)
+{
+	std::cout << "char: impossible\n";
+	std::cout << "int: impossible\n";
+	std::cout << "float: " << pseudos.p_float << '\n';
+	std::cout << "double: " << pseudos.p_double << '\n';
 }
 
-static bool int_convert(const std::string& input)
+static bool pseudo(const std::string &input)
 {
-	std::istringstream iss;
-	int output;
+	static const t_pseudo pseudos[3] = { "-inff" , "-inf" , "+inff", "+inf", "nanf", "nan" };
 
-	iss.str(input);
-	iss >> output;
-	if (iss.fail() || iss.eof())
-		return (false);
-
-	if (std::isprint(output))
-		std::cout << "char: " << output << '\n';
-	else
-		std::cout << "char: Non displayable\n";
-
-	if (static_cast<long long>(output) > std::numeric_limits<int>::max() || static_cast<long long>(output) < std::numeric_limits<int>::min())
-		std::cout << "int: impossible\n";
-	else
-		std::cout << "int: " << static_cast<int>(output) << '\n';
-
-	if (static_cast<long double>(output) > std::numeric_limits<float>::max() || static_cast<long double>(output) < std::numeric_limits<float>::min())
-		std::cout << "float: nanf\n";
-	else
-		std::cout << "float: " << static_cast<float>(output) << '\n';
-		
-	if (static_cast<long double>(output) > std::numeric_limits<double>::max() || static_cast<long double>(output) < std::numeric_limits<double>::min())
-		std::cout << "double: nan\n";
-	else
-		std::cout << "double: " << static_cast<double>(output) << '\n';
-	return (true);
-}
-
-static bool float_convert(const std::string &input)
-{
-	std::istringstream iss;
-	float output;
-
-	iss.str(input);
-	iss >> output;
-	if (iss.fail())
-		return (false);
-	
-	if (std::isprint(output))
-		std::cout << "char: " << output << '\n';
-	else
-		std::cout << "char: Non displayable\n";
-
-	if (static_cast<long long>(output) > std::numeric_limits<int>::max() || static_cast<long long>(output) < std::numeric_limits<int>::min())
-		std::cout << "int: impossible\n";
-	else
-		std::cout << "int: " << static_cast<int>(output) << '\n';
-
-	if (static_cast<long double>(output) > std::numeric_limits<float>::max() || static_cast<long double>(output) < std::numeric_limits<float>::min())
-		std::cout << "float: nanf\n";
-	else
-		std::cout << "float: " << static_cast<float>(output) << '\n';
-		
-	if (static_cast<long double>(output) > std::numeric_limits<double>::max() || static_cast<long double>(output) < std::numeric_limits<double>::min())
-		std::cout << "double: nan\n";
-	else
-		std::cout << "double: " << static_cast<double>(output) << '\n';
-}
-
-static bool double_convert(const std::string &input)
-{
-	std::istringstream iss;
-	double output;
-
-	iss.str(input);
-	iss >> output;
-	if (iss.fail())
-		std::cout << "double: nan\n";
-	else
-		std::cout << "double: " << output << '\n';
+	for (size_t i = 0; i < (sizeof(pseudos)/sizeof(*pseudos)); i++)
+	{
+		if (pseudos[i].p_float == input || pseudos[i].p_double == input)
+			return (print_pseudo(pseudos[i]), true);
+	}
+	return (false);
 }
 
 void ScalarConverter::convert(const std::string& input)
 {
-	if (char_convert(input))
-		return;
-	if (int_convert(input))
-		return;
-	if (input.back() == 'f')
+	// Check if special case
 
-	if (float_convert(input))
+	if (pseudo(input))
 		return;
-	if (double_convert(input))
-		return;
-	
-	std::cout << "Not anything\n";
+	// If not special case
+	// Check which type it is
+
+
 }
 
 ScalarConverter::ScalarConverter() { }
-ScalarConverter::ScalarConverter(const ScalarConverter& other) { }
-ScalarConverter& ScalarConverter::operator= (const ScalarConverter& other) { return (*this); }
+ScalarConverter::ScalarConverter(const ScalarConverter& other) { (void)other; }
+ScalarConverter& ScalarConverter::operator= (const ScalarConverter& other) { return ((void)other, *this); }
 ScalarConverter::~ScalarConverter() { }
--- 42/cpp06/ex00/main.cpp
+++ 42/cpp06/ex00/main.cpp
@@ -1,8 +1,10 @@
 #include "ScalarConverter.hpp"
-#include <iostream> // again?
+#include <iostream>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	
+	if (argc != 2)
+		return (std::cout << "Invalid args\n", 1);
+	ScalarConverter::convert(argv[1]);
+	return (0);
 }
-
