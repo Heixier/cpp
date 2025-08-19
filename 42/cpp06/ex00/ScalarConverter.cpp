6b8a1ebb319221627b58fc4d479e327e2a0d010f
Heixier <rzsiah@gmail.com>
2025-08-19 12:48:37 +0800

    add a bit more

--- 42/cpp06/ex00/ScalarConverter.cpp
+++ 42/cpp06/ex00/ScalarConverter.cpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 #include <iomanip>
 
+// Uses references to strings make sure this is static
 typedef struct s_pseudo
 {
 	const std::string& p_float;
@@ -26,22 +27,32 @@ static bool pseudo(const std::string &input)
 
 	for (size_t i = 0; i < (sizeof(pseudos)/sizeof(*pseudos)); i++)
 	{
-		if (pseudos[i].p_float == input || pseudos[i].p_double == input)
+		if (input == pseudos[i].p_float || input == pseudos[i].p_double)
 			return (print_pseudo(pseudos[i]), true);
 	}
+	return (false);
+}
+
+static bool try_char(const std::string &input)
+{
+	(void)input;
 	return (false);
 }
 
 void ScalarConverter::convert(const std::string& input)
 {
 	// Check if special case
-
 	if (pseudo(input))
 		return;
+
 	// If not special case
 	// Check which type it is
-
-
+	if (try_char(input))
+		return;
+	
+	static const std::string types[4] = { "char", "int", "float", "double" };
+	for (int i = 0; i < 4; i++)
+		std::cout << types[i] << ": impossible\n";
 }
 
 ScalarConverter::ScalarConverter() { }
