d68ff77f5a07544753b14438354a2cfac96af988
Heixier <rzsiah@gmail.com>
2025-08-21 16:21:14 +0800

    show memory address for swap

--- 42/cpp07/ex00/info.hpp
+++ 42/cpp07/ex00/info.hpp
@@ -11,8 +11,13 @@ void info(T& first, T& second)
 	std::cout << "\nLowest value: " << (&(min(first, second)) == &first ? "first" : "second") << '\n';
 	std::cout << "Highest value: " << (&(max(first, second)) == &first ? "first" : "second") << '\n';
 
-	std::cout << "\nSwapping first and second!\n";
+
+	std::cout << "\nSwapping values of first and second: \n";
+	std::cout << "\nfirst addr: " << &first << "\nsecond addr: " << &second << '\n';
+
 	swap(first, second);
+	std::cout << "\nMake sure only values (not addresses) were swapped:\n";
+	std::cout << "first addr: " << &first << "\nsecond addr: " << &second << '\n';
 	std::cout << "\nfirst: " << first << " second: " << second << '\n';
 
 	std::cout << "\nLowest value: " << (&(min(first, second)) == &first ? "first" : "second") << '\n';
