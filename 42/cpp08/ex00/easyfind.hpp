5d4feb74fe7071b957114c85595a4f8dacf2c5c2
Heixier <rzsiah@gmail.com>
2025-09-10 02:40:21 +0800

    add easyfind tested a bit

--- 42/cpp08/ex00/easyfind.hpp
+++ 42/cpp08/ex00/easyfind.hpp
@@ -5,7 +5,7 @@
 template<typename T>
 int easyfind(const T& int_container, const int target)
 {
-	if (std::find(int_container.begin(), int_container.end(), target) == int_container.end)
+	if (std::find(int_container.begin(), int_container.end(), target) == int_container.end())
 		throw std::exception();
 	return (target);
 }
--- 42/cpp08/ex00/main.cpp
+++ 42/cpp08/ex00/main.cpp
@@ -5,7 +5,7 @@
 
 void print_vect(const std::vector<int>& vect)
 {
-	for (int i = 0; i < vect.size(); i++)
+	for (size_t i = 0; i < vect.size(); i++)
 		std::cout << vect[i] << '\n';
 }
 
@@ -14,4 +14,13 @@ int main()
 	static const int arr[] = {1, 3, 5, 7, 9};
 	std::vector<int> vect(arr, arr + sizeof(arr) / sizeof(arr[0]));
 	print_vect(vect);
+
+	try
+	{
+		std::cout << easyfind(vect, 6);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception caught!\n";
+	}
 }
